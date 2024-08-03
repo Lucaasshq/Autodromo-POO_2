aa3763b55caf791abf3348632c2d5dabdb539bd2
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-03 12:39:09 -0300

    initial commit

---
+++ src/Autodromo.java
@@ -0,0 +1,18 @@
+
+
+public class Autodromo {
+
+	public static void main(String[] args) {
+		
+		Carro jeep = new Carro();
+		jeep.setChassi("987546");
+		jeep.ligar();
+		
+		Moto z400 = new Moto();
+		z400.setChassi("78965");
+		z400.ligar();
+	}
+
+
+
+}
